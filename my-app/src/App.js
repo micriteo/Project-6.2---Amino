0301da8093664dae2f72d1d2fb9cf138ffb51289
Costache Florian-Alin <91316565+Fluffkin23@users.noreply.github.com>
2023-05-18 13:04:50 +0200

    Code indexed and desktop version

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,14 +8,15 @@ import background from './background_animo.jpeg';
 
 
 function App() 
-{ //Define what should happen when the div is clicked
+{ 
+  //Define what should happen when the div is clicked
   const handleButtonClick = () =>
   {
     alert("Button clicked!");
   }
+
   return (
     <div className="App">
-
       <header className="App-header">
         <h1>Welcome to your Animo!</h1>
         <img src= {animo_logo} alt="Animo" className="logo-img" />
@@ -23,8 +24,7 @@ function App()
 
       <main>
         <p className = "header_campaign">The new way of ordering in safety! </p>
-
-        <div className="rounded-rectangle-wrapper">  {/* New wrapper div */}
+        <div className="rounded-rectangle-wrapper"> 
           <div className="rounded-rectangle" onClick={handleButtonClick}>
             <img src={coffe_vector} alt="Capuccino" className="capuccino-img" />
             <p className = "capucino-text">Capucino </p>
@@ -33,46 +33,29 @@ function App()
             <img src={tea_vector} className="capuccino-img" />
             <p className = "capucino-text">Tea </p>
           </div>
-
           <div className="rounded-rectangle" onClick={handleButtonClick}>
             <img src={hot_water_vector} alt="Capuccino" className="capuccino-img" />
             <p className = "capucino-text">Hot water </p>
           </div>
         </div>
-
-      <div className="coffee-selection">
-        <div className="coffee-image">
-          {/* The background image will be added via CSS */}
+        <div className="coffee-selection">
+          <div className="coffee-image"></div> { /* background added via CSS */}
+          <p className = "coffe-text">Capucino</p>
+          <p className = "coffe-price"> Free</p>
+          <div className="ellipse" onClick={handleButtonClick}>
+            <p className="plus">+</p> {/* New "+" text inside the ellipse */}
+          </div>
         </div>
-
+      <div className="coffee-selection">
+        <div className="hot_chocolate-image"></div> {/* The background image will be added via CSS */}
         <p className = "coffe-text">Capucino</p>
         <p className = "coffe-price"> Free</p>
         <div className="ellipse" onClick={handleButtonClick}>
           <p className="plus">+</p> {/* New "+" text inside the ellipse */}
-
         </div>
-
       </div>
-
       <div className="coffee-selection">
-        <div className="hot_chocolate-image">
-          {/* The background image will be added via CSS */}
-        </div>
-
-        <p className = "coffe-text">Capucino</p>
-        <p className = "coffe-price"> Free</p>
-        <div className="ellipse" onClick={handleButtonClick}>
-          <p className="plus">+</p> {/* New "+" text inside the ellipse */}
-
-        </div>
-
-      </div>
-
-      <div className="coffee-selection">
-        <div className="capucino-image">
-          {/* The background image will be added via CSS */}
-        </div>
-
+        <div className="capucino-image"></div> {/* The background image will be added via CSS */}
         <p className = "coffe-text">Capucino</p>
         <p className = "coffe-price"> Free</p>
         <div className="ellipse" onClick={handleButtonClick}>
