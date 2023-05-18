8fcaef161da4808c400cddfbec6c006e80b1938f
Andreikova <112638712+Andreikova@users.noreply.github.com>
2023-05-18 18:42:20 +0200

    Made the one button for permissions, recording and ending recording.
    Made the button interactive - hovering and click / click and hold effects for visual response to user actions.
    
    Javascript altered to fit this new button.
    
    CSS altered to make the visual responsiveness of the button.
    
    TODO: Test

--- SirenSmth/static/script.js
+++ SirenSmth/static/script.js
@@ -1,61 +1,87 @@
-'use strict'
+'use strict';
 
-let log = console.log.bind(console), id = val => document.getElementById(val),
-    ul = id('ul'), gUMbtn = id('gUMbtn'),
-    start = id('start'),
-    stop = id('stop'),
-    label = id('label'),
-    stream, recorder, counter = 1, chunks, media;
+const recordButton = document.getElementById('recordButton');
+let recorder, chunks;
 
-
-gUMbtn.onclick = e => {
-    console.log('gUMbtn clicked');
+recordButton.onclick = (e) => {
     let recType = {
         audio: {
-            tag: 'audio', type: 'audio/wav', ext: '.wav', gUM: {audio: true}
+            tag: 'audio',
+            type: 'audio/wav',
+            ext: '.wav',
+            gUM: { audio: true }
         }
     };
-    media = recType.audio;
-    navigator.mediaDevices.getUserMedia(media.gUM).then(_stream => {
-        stream = _stream;
-        id('gUMArea').style.display = 'none';
-        id('btns').style.display = 'inherit';
-        start.removeAttribute('disabled');
-        recorder = new MediaRecorder(stream);
-        recorder.ondataavailable = e => {
-            chunks.push(e.data);
-            if (recorder.state === 'inactive') makeLink();
-        };
-    }).catch(log);
+    const media = recType.audio;
+    navigator.mediaDevices.getUserMedia(media.gUM)
+        .then(_stream => {
+            const stream = _stream;
+            recorder = new MediaRecorder(stream);
+            chunks = [];
+            recorder.ondataavailable = (e) => {
+                chunks.push(e.data);
+                if (recorder.state === 'inactive') {
+                    makeLink();
+                }
+            };
+            recorder.start();
+        })
+        .catch(log);
+};
+
+class ClickAndHold {
+    constructor(target, callback) {
+        this.target = target;
+        this.callback = callback;
+        this.isHeld = false;
+        this.activeHoldTimeoutId = null;
+
+        // So it works with desktop and mobile
+        ['mousedown', 'touchstart'].forEach(type => {
+            this.target.addEventListener(type, this._onHoldStart.bind(this));
+        });
+
+        ['mouseup', 'mouseleave', 'mouseout', 'touchend', 'touchcancel'].forEach(type => {
+            this.target.addEventListener(type, this._onHoldEnd.bind(this));
+        });
+    }
+
+    _onHoldStart() {
+        this.isHeld = true;
+        this.activeHoldTimeoutId = setTimeout(() => {
+            if (this.isHeld) {
+                this.callback();
+            }
+        }, 100);
+    }
+
+    _onHoldEnd() {
+        this.isHeld = false;
+        clearTimeout(this.activeHoldTimeoutId);
+    }
 }
 
-start.onclick = e => {
-    start.disabled = true;
-    stop.removeAttribute('disabled');
-    chunks = [];
-    recorder.start();
-}
-
-stop.onclick = e => {
-    stop.disabled = true;
-    recorder.stop();
-    start.removeAttribute('disabled');
-}
+let log = console.log.bind(console),
+    id = val => document.getElementById(val),
+    label = id('label');
 
 function makeLink() {
-    let blob = new Blob(chunks, {type: media.type})
+    let blob = new Blob(chunks, { type: 'audio/wav' });
     const formData = new FormData();
-    formData.append('audio_file', blob, `sound${media.ext}`);
+    formData.append('audio_file', blob, 'sound.wav');
 
     fetch('/converter', {
         method: 'POST',
         body: formData
-    })
-        .then(async response => {
-            if (response.ok) {
-                label.innerHTML = await response.text();
-            } else {
-                console.log("HTTP-Error: " + response.status);
-            }
-        })
+    }).then(async response => {
+        if (response.ok) {
+            label.innerHTML = await response.text();
+        } else {
+            console.log('HTTP-Error: ' + response.status);
+        }
+    });
 }
+
+new ClickAndHold(recordButton, () => {
+    console.log('Press and hold button action');
+});
