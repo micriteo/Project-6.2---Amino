e3be1c85d7d3cdfddf6258de2a7666cd558a1a6e
micriteo <76212872+micriteo@users.noreply.github.com>
2023-05-19 11:07:27 +0200

    Desktop Version Implemented

--- animo-website/src/HomePage.js
+++ animo-website/src/HomePage.js
@@ -6,13 +6,17 @@ import logo from './Assets/Logo.png';
 function HomePage() {
     return (
         <div className="home-container">
-            <img className="background-image" src={backgroundImage} alt="Background" />
-            <h1 className="title">Welcome to your Animo</h1>
-            <img className="logo" src={logo} alt="Logo" />
-            <div className="button-container">
-                <button className="voice-button">Order via Voice</button>
-                <button className="select-button">Order via Selecting</button>
-            </div>
+            <header className="App-header">
+                <img className="background-image" src={backgroundImage} alt="Background"/>
+                <div className="logo-title-container">
+                    <h1 className="title">Welcome to your Animo</h1>
+                    <img className="logo" src={logo} alt="Logo"/>
+                </div>
+                <div className="button-container">
+                    <button className="voice-button">Order via voice</button>
+                    <button className="select-button">Order via selecting</button>
+                </div>
+            </header>
         </div>
     );
 }
