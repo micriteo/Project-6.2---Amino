99d7dfd6e471820c0d4f6403f4f58c9595161938
Miroslav Penchev <miroslav.penchev@student.nhlstenden.com>
2023-05-31 09:17:54 +0200

    order recognition

--- SirenSmth/app.py
+++ SirenSmth/app.py
@@ -7,12 +7,17 @@ from flask import Flask, request, render_template, send_from_directory
 import speech_recognition as sr
 
 app = Flask(__name__)
+turn = 0
+current_coffee = None
+coffeeTypes = ["coffee", "expresso", "espresso", "milk coffee", "cappuccino",
+               "coffee chocolate", "chocolate coffee", "chocolate milk", "hot chocolate", "hot water",
+               "double expresso", "latte macchiato", "wiener melange"]
 
 
 @app.route('/')
 def home():
     # subprocess.call("../animo-website npm start", shell=True)
-    return render_template("bndex.html")
+    return render_template("bindex.html")
 
 
 @app.route('/converter', methods=['POST'])
@@ -23,6 +28,7 @@ async def converter():
         convert_to_wav()
         text = await convert_to_text()
         remove_files()
+        text = handle_coffee_order(text)
         text_to_speech(text)
         return text
 
@@ -54,11 +60,11 @@ def remove_files():
 
 def text_to_speech(text):
     voice_dict = {'Male': 0, 'Female': 1}
-    code = voice_dict[0]
+    code = voice_dict['Male']
     engine = pyttsx3.init()
 
     # Setting up voice rate
-    engine.setProperty('rate', 125)
+    engine.setProperty('rate', 150)
 
     # Setting up volume level between 0 and 1
     engine.setProperty('volume', 0.8)
@@ -69,6 +75,35 @@ def text_to_speech(text):
 
     engine.say(text)
     engine.runAndWait()
+
+
+def handle_coffee_order(voice_text):
+    global turn
+    global current_coffee
+
+    if turn == 0:
+        for coffee in coffeeTypes:
+            if coffee in voice_text:
+                current_coffee = coffee
+                break
+        if current_coffee:
+            turn = 1
+            return f"You have requested a {current_coffee}. Is this correct?"
+        else:
+            return "I'm sorry, I could not understand you. What coffee would you like?"
+
+    elif turn == 1:
+        if "yes" in voice_text or "sounds good" in voice_text or "sure" in voice_text:
+            turn = 0
+            coffee = current_coffee
+            current_coffee = None
+            return f"Brewing {coffee} now!"
+        elif "no" in voice_text or "nope" in voice_text or "cancel" in voice_text:
+            turn = 0
+            current_coffee = None
+            return "What coffee would you like instead?"
+        else:
+            return f"I'm sorry, I could not understand you. Would you like a {current_coffee}? Please say Yes or No"
 
 
 if __name__ == '__main__':
