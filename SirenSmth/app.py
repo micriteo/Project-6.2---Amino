d66cc4d68be988a8726102fe37119a97a9f1a456
Miroslav Penchev <miroslav.penchev@student.nhlstenden.com>
2023-06-04 00:34:55 +0200

    raspberry pi paths

--- SirenSmth/app.py
+++ SirenSmth/app.py
@@ -28,7 +28,7 @@ def home():
 async def converter():
     if request.method == 'POST':
         audio_file = request.files['audio_file']
-        audio_file.save(r'C:\MainStuff\Uni\Project6.2\new_voice.webm')
+        audio_file.save(r'/home/animo/AnimoMisc/new_voice.webm')
         convert_to_wav()
         text = await convert_to_text()
         remove_files()
@@ -39,7 +39,7 @@ async def converter():
 
 async def convert_to_text():
     r = sr.Recognizer()
-    with sr.AudioFile(r'C:\MainStuff\Uni\Project6.2\new_voice.wav') as source:
+    with sr.AudioFile(r'/home/animo/AnimoMisc/new_voice.wav') as source:
         audio_data = r.record(source)
         try:
             text = r.recognize_google(audio_data, language="nl-NL")
@@ -49,17 +49,17 @@ async def convert_to_text():
 
 
 def convert_to_wav():
-    input_path = r'C:\MainStuff\Uni\Project6.2\new_voice.webm'
-    output_path = r'C:\MainStuff\Uni\Project6.2\new_voice.wav'
-    ffmpeg_path = r"C:\MainStuff\ffmpeg-6.0\bin\ffmpeg.exe"
+    input_path = r'/home/animo/AnimoMisc/new_voice.webm'
+    output_path = r'/home/animo/AnimoMisc/new_voice.wav'
+    ffmpeg_path = r"ffpeg"
     subprocess.call([ffmpeg_path, '-i', input_path, output_path])
 
 
 def remove_files():
     extensions = ['.wav', '.webm']
     for ext in extensions:
-        if os.path.exists(r'C:\MainStuff\Uni\Project6.2\new_voice' + ext):
-            os.remove(r'C:\MainStuff\Uni\Project6.2\new_voice' + ext)
+        if os.path.exists(r'/home/animo/AnimoMisc/new_voice' + ext):
+            os.remove(r'/home/animo/AnimoMisc/new_voice' + ext)
 
 
 def text_to_speech(text):
