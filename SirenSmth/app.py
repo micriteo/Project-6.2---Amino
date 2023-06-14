78d57a718290f29119302378d9c3fde26929a406
kaiseraftab <91316936+kaiseraftab@users.noreply.github.com>
2023-06-14 14:21:18 +0200

    images added

--- SirenSmth/app.py
+++ SirenSmth/app.py
@@ -79,7 +79,7 @@ def store_order(order):
 
 def send_order_to_android_list(order):
     # Define the IP address and port of the Android application running on the same server
-    android_ip = '172.20.10.5'  # this need to be change everytime the android device is connected to a new network
+    android_ip = '141.252.206.171'  # this need to be change everytime the android device is connected to a new network
     android_port = 9999  # Replace with the port number the Android application is listening on
 
     try:
